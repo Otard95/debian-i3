8cdfc54d9c5eb7d2d80e3d330e4b37bd46aa22c2
StianM <stian.myklebostad@schibsted.com>
2021-12-22 10:00:59 +0100

    Creat setup script for NetworkManager

---
+++ setup-network-manager.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo systemctl enable systemd-resolved
+sudo systemctl restart systemd-resolved
+sudo systemctl enable NetworkManager
+sudo systemctl restart  NetworkManager
+
+nmcli device wifi list 
+echo "Use the following command to connect to your wifi network"
+echo "nmcli device wifi connect <network name> password <password>"
