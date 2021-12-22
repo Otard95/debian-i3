dd09e4964635a1e0b80090984bab2a26073e4c29
StianM <stian.myklebostad@schibsted.com>
2021-12-22 12:26:03 +0100

    Install curl thefuck cli and brave

--- install.sh
+++ install.sh
@@ -13,10 +13,18 @@ pacages=( \
   kitty \
   neofetch \
   # Tools
+  curl
   vim \
   git \
+  python3-dev \
+  python3-pip \
+  python3-setuptools \
   # Fonts
   fonts-firacode \
 )
 
 sudo apt update && sudo apt upgrade -y && sudo apt install -y ${pacages[@]}
+
+pip3 install --user thefuck
+
+./install-brave.sh
