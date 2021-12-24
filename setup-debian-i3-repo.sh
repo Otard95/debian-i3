4a571cd22549d6f3d4c9f59f39ab6baf5d470b1d
StianM <stian.myklebostad@schibsted.com>
2021-12-24 13:34:43 +0100

    Fix cd in setup

--- setup-debian-i3-repo.sh
+++ setup-debian-i3-repo.sh
@@ -7,7 +7,7 @@ backDir=$(pwd)
 sub_header "git clone debian-i3 repo"
 cd ~/
 git clone https://github.com/Otard95/debian-i3.git
-cd ~/dotfiles
+cd ~/debian-i3
 
 sub_header "Setup git remotes"
 git remote set-url origin git@github.com:Otard95/debian-i3.git
