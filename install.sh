2733e59103ba5f099114faaac10d517cc4bd99a5
StianM <stian.myklebostad@schibsted.com>
2021-12-24 12:11:36 +0100

    Fix snap install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacages=( \
+packages=( \
   # Core
   xorg \
   i3 \
@@ -21,21 +21,37 @@ pacages=( \
   fonts-firacode \
 )
 after_install=()
+dry_run=0
+
+for arg in $@; do
+  if [[ $arg == "--dry" ]]; then
+    dry_run=1
+  fi
+done
 
 source ./utils.sh
 
 read -p "Setup snap? (Y/n): " answer
-if [[ -z "$answer" ]] || [[ $answer =~ ^[Nn]o?$ ]]; then
-  pacages+=("snapd")
+if [[ -z "$answer" ]] || [[ $answer =~ ^[Yy](es)?$ ]]; then
+  packages+=("snapd")
   after_install+=("./setup-snap.sh")
 fi
 
-sub_header "Install packages"
-sudo apt update && sudo apt upgrade -y && sudo apt install -y ${pacages[@]}
+if [[ $dry_run -ne 0 ]]; then
+  echo "Packages: ${packages[@]}"
+  echo "After Install: ${after_install[@]}"
+else
+  sub_header "Install packages"
+  sudo apt update && sudo apt upgrade -y && sudo apt install -y ${packages[@]}
+fi
 
 if [[ ${#after_install[@]} -gt 0 ]]; then
   sub_header "Post install"
   for action in "${after_install[@]}"; do
-    eval "$action"
+    if [[ $dry_run -ne 0 ]]; then
+      echo "Post install run: $action"
+    else
+      eval "$action"
+    fi
   done
 fi
